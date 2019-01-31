b1d4c9dee133e6d9c7c0ea05cb15649664a529a5
mj <onyangomaureen95@gmail.com>
2019-01-31 23:21:06 +0300

    create user view in rumbler web views

---
+++ lib/rumbler_web/views/user_view.ex
@@ -0,0 +1,11 @@
+defmodule RumblerWeb.UserView do
+    use RumblerWeb, :view
+    
+    alias Rumbler.Accounts
+
+    def first_name(%Accounts.User{name: name}) do
+        name
+        |>String.split("")
+        |>Enum.at(0)
+    end
+end
