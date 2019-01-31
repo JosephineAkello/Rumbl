57f0dc27734f728623baad137c1a6141c4df097f
mj <onyangomaureen95@gmail.com>
2019-01-31 22:56:26 +0300

    Create user controllrer

---
+++ lib/rumbler_web/controllers/user_controller.ex
@@ -0,0 +1,11 @@
+defmodule RumberWeb.UserController do
+    use RumblerWeb, :controller
+
+    alias Rumbler.Accounts
+
+    @spec index(Plug.Conn.t(), any()) :: Plug.Conn.t()
+    def index(conn, _params)do
+        users=Accounts.list_users()
+        render(conn, "index.html", users: users)
+    end
+end
