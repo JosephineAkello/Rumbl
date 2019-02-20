5d715579ccac8cdb8a8281fea3f0f140306c5dd7
mj <onyangomaureen95@gmail.com>
2019-02-20 21:27:22 +0300

    add show function to user controller

--- lib/rumbler_web/controllers/user_controller.ex
+++ lib/rumbler_web/controllers/user_controller.ex
@@ -8,4 +8,9 @@ defmodule RumblerWeb.UserController do
     users = Accounts.list_users()
     render( conn, "index.html", users: users)
   end
+
+  def show(conn, %{"id"=> id}) do
+    user=Accounts.get_user(id)
+    render(conn, "show.html", users: user)
+end
 end
