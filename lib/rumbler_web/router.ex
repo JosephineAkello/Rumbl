7c9e60460f1e414c36f99b4dcd22ff04cee71fe1
mj <onyangomaureen95@gmail.com>
2019-04-12 17:13:39 +0300

    add resources to router

--- lib/rumbler_web/router.ex
+++ lib/rumbler_web/router.ex
@@ -16,9 +16,8 @@ defmodule RumblerWeb.Router do
   scope "/", RumblerWeb do
     pipe_through :browser
 
-    get "/users", UserController, :index
-    get "/users/:id", UserController, :show
     get "/", PageController, :index
+resources "/users", UserController, only: [:index, :show, :new, :create]
   end
 
   # Other scopes may use custom stacks.
